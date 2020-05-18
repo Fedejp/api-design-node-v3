7b42a21d73fae152b29344cb9a529eb45cf46594
Fede Parodi <federico.parodi@sovanta.com>
2020-05-18 18:18:24 -0300

    done lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,17 @@
 import { Router } from 'express'
+import { crudControllers } from '../../utils/crud'
 
 const router = Router()
+
+router
+  .route('/')
+  .get(crudControllers.getOne)
+  .post(crudControllers.createOne)
+
+router
+  .route('/:id')
+  .get(crudControllers.getOne)
+  .put(crudControllers.updateOne)
+  .delete(crudControllers.deletOne)
 
 export default router
