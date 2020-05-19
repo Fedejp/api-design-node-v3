dcfe73337dca8f6763337c8372ac62f4fee9ee76
Fede Parodi <federico.parodi@sovanta.com>
2020-05-19 17:11:18 -0300

    lesson 3 completed

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,34 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'complete', 'pastdue'], // No solo required, sino que tambien tiene que ser una de esas tres cosas
+      default: 'active' // uno de los enum
+    },
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId, // este campo tiene el ID de un documento 'ref'
+      ref: 'user', // el nombre del model que referencia
+      required: true
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'list',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+itemSchema.index({ list: 1, item: 1 }, { unique: true }) // Genero un indice compuesto para que haya solo un item con un cierto nombre por lista. El orden aca importa.
 export const Item = mongoose.model('item', itemSchema)
