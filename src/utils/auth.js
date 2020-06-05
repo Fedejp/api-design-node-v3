c576335ab8dada07e59c16194c2b225eadb461c8
Fede Parodi <federico.parodi@sovanta.com>
2020-06-05 18:27:05 -0300

    done :cool:

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,64 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  // signup and signin cannot be protected from outsiders, because outsiders have to use it to log in into your API.
+  // Therefore, you cannot use proper REST to create these routes
+  if (!req.body.email || !req.body.password) {
+    // For the test to pass, we have to check for the req to have email and pwd
+    return res.status(400).send({ message: 'Email and password required' })
+  }
+  try {
+    const user = await User.create(req.body)
+    const token = newToken(user)
+    return res.status(201).send({ token }) // it went well you can create your user
+  } catch (e) {
+    console.error(e) // here we're checking if anything went wrong, like if there's already an user with that username or email
+    return res.status(400).end()
+  }
+}
 
-export const signin = async (req, res) => {}
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    // For the test to pass, we have to check for the req to have email and pwd
+    return res.status(400).send({ message: 'Email and password required' })
+  }
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) {
+    // If it's a random email and itsn't in our database
+    return res.status(401).send({ message: 'Not an user' })
+  }
+  try {
+    const match = user.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'Wrong password' })
+    }
+    const token = newToken(user)
+    return res.status(201).send({ token }) // If everything is fine, sends a new token
+  } catch (e) {
+    console.error(e) // if there's no password match.
+    return res.status(400).send({ message: 'Wrong password' })
+  }
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  if (!req.headers.authorization) {
+    return res.status(401).end()
+  }
+  let token = req.headers.authorization.split('Baerer ')[1] // This is because JWT gives the token as "Bearer [token]" and we just care about the token
+  if (!token) {
+    return res.status(401).end()
+  }
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean() // converts to json
+      .exec()
+    req.user = user
+    next()
+  } catch (e) {
+    console.error(e)
+    return res.status(400).end()
+  }
 }
